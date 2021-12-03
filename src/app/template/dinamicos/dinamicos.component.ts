c47f0f152336576c9e335e7acee2f6752ed75104
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2021-12-03 19:27:14 +0100

    Completada parte de dinámicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,23 +1,50 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
   @ViewChild('miFormulario') miFormulario!: NgForm;
 
-  constructor() { }
+  nuevoJuego: string = '';
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Carlos',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear' },
+      { id: 2, nombre: 'Death Stranding' }
+    ]
+  }
+
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
   }
 
   guardar() {
     console.log(this.miFormulario);
+  }
+
+  eliminar(index:number) {
+    this.persona.favoritos.splice(index, 1);
   }
 
 
